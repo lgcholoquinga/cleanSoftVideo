3693a57af4106da66bdcf79a9643847b1b026aad
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-03-31 11:19:06 -0500

    Project initial cleansoftVideo

---
+++ src/components/Hola.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hola = () => {
+    return (
+        <h1>Gola!!sss</h1>
+    );
+};
+
+export default Hola;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './assets/styles/Index.scss';
+
+ReactDom.render(<App />, document.getElementById('root'));
